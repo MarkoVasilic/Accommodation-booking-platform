b729604e59eba35580ee7394d4cd5a52b59d7657
NadjaKanjuh <nadjakanjuh00@gmail.com>
2023-05-02 18:46:44 +0200

    get all reservations

--- accommodation_reservation_app/reservation_service/repository/reservation_repository.go
+++ accommodation_reservation_app/reservation_service/repository/reservation_repository.go
@@ -1,9 +1,31 @@
 package repository
 
 import (
+	"context"
+	"time"
 	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/bson"
+	"github.com/MarkoVasilic/Accommodation-booking-platform/accomodation_reservation_app/reservation_service/models"
 )
 
 type ReservationRepository struct {
 	ReservationCollection *mongo.Collection
+}
+
+
+func (repo *ReservationRepository) GetAllReservations() ([]models.Reservation, error) {
+	var ctx, cancel = context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	cursor, err := repo.ReservationCollection.Find(ctx, bson.M{})
+	if err != nil {
+		return nil, err
+	}
+
+	var reservations []models.Reservation
+	if err = cursor.All(ctx, &reservations); err != nil {
+		return nil, err
+	}
+
+	return reservations, nil
 }
--- accommodation_reservation_app/reservation_service/service/reservation_service.go
+++ accommodation_reservation_app/reservation_service/service/reservation_service.go
@@ -2,8 +2,17 @@ package service
 
 import (
 	"github.com/MarkoVasilic/Accommodation-booking-platform/accomodation_reservation_app/reservation_service/repository"
+	"github.com/MarkoVasilic/Accommodation-booking-platform/accomodation_reservation_app/reservation_service/models"
 )
 
 type ReservationService struct {
 	ReservationRepository *repository.ReservationRepository
+}
+
+func (svc *ReservationService) GetAllReservations() ([]models.Reservation, error) {
+	reservations, err := svc.ReservationRepository.GetAllReservations()
+	if err != nil {
+		return nil, err
+	}
+	return reservations, nil
 }
