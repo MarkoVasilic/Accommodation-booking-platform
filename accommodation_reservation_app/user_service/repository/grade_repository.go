16ada0290afd2607a6bca2bce4671b31a11881fe
NadjaKanjuh <nadjakanjuh00@gmail.com>
2023-06-04 11:25:16 +0200

    ocenjivanje hosta

--- accommodation_reservation_app/user_service/repository/grade_repository.go
+++ accommodation_reservation_app/user_service/repository/grade_repository.go
@@ -1,9 +1,55 @@
 package repository
 
 import (
+	"context"
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+
+	"github.com/MarkoVasilic/Accommodation-booking-platform/accomodation_reservation_app/user_service/models"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
 type GradeRepository struct {
 	GradeCollection *mongo.Collection
+}
+
+func (repo *GradeRepository) CreateUserGrade(userGrade *models.UserGrade) error {
+	var ctx, cancel = context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	userGrade.ID = primitive.NewObjectID()
+	_, inserterr := repo.GradeCollection.InsertOne(ctx, userGrade)
+	return inserterr
+}
+
+func (repo *GradeRepository) UpdateUserGrade(userGrade *models.UserGrade) error {
+	var ctx, cancel = context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	filter := bson.M{"_id": userGrade.ID}
+	update := bson.M{
+		"$set": bson.M{
+			"grade": userGrade.Grade,
+		},
+	}
+
+	_, updateErr := repo.GradeCollection.UpdateOne(ctx, filter, update)
+	return updateErr
+}
+
+func (repo *GradeRepository) GetGradeById(id primitive.ObjectID) (models.UserGrade, error) {
+	var ctx, cancel = context.WithTimeout(context.Background(), 5*time.Second)
+	var userGrade models.UserGrade
+	err := repo.GradeCollection.FindOne(ctx, bson.M{"_id": id}).Decode(&userGrade)
+	defer cancel()
+	return userGrade, err
+}
+
+func (repo *GradeRepository) DeleteUserGrade(id primitive.ObjectID) (*mongo.DeleteResult, error) {
+	var ctx, cancel = context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	return repo.GradeCollection.DeleteOne(ctx, bson.M{"_id": id})
 }
--- accommodation_reservation_app/user_service/service/grade_service.go
+++ accommodation_reservation_app/user_service/service/grade_service.go
@@ -1,9 +1,88 @@
 package service
 
 import (
+	"context"
+	"time"
+
+	"github.com/MarkoVasilic/Accommodation-booking-platform/accomodation_reservation_app/user_service/models"
 	"github.com/MarkoVasilic/Accommodation-booking-platform/accomodation_reservation_app/user_service/repository"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 type GradeService struct {
 	GradeRepository *repository.GradeRepository
+}
+
+func (service *GradeService) CreateUserGrade(userGrade models.UserGrade) (string, error) {
+	var _, cancel = context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	validationErr := Validate.Struct(userGrade)
+	if validationErr != nil {
+		err := status.Errorf(codes.InvalidArgument, "user grade fields are not valid")
+		return "User grade fields are not valid", err
+	}
+
+	inserterr := service.GradeRepository.CreateUserGrade(&userGrade)
+	if inserterr != nil {
+		err := status.Errorf(codes.Internal, "failed to create user grade")
+		return "Failed to create user grade", err
+	}
+
+	return "Successfully created user grade", nil
+}
+
+func (service *GradeService) UpdateUserGrade(id string, grade int) (string, error) {
+	objectID, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		err := status.Errorf(codes.InvalidArgument, "Invalid ID format")
+		return "Invalid ID format", err
+	}
+
+	//provera da li postoji ocena
+	_, err = service.GradeRepository.GetGradeById(objectID)
+	if err != nil {
+		err := status.Errorf(codes.NotFound, "There is no grade with that ID")
+		return "There is no grade with that ID", err
+	}
+
+	//izmeni ocenu
+	userGrade := &models.UserGrade{
+		ID:    objectID,
+		Grade: grade,
+	}
+
+	err = service.GradeRepository.UpdateUserGrade(userGrade)
+	if err != nil {
+		err := status.Errorf(codes.Internal, "Failed to update grade")
+		return "Failed to update grade", err
+	}
+
+	return "Successfully updated grade", nil
+}
+
+func (service *GradeService) DeleteUserGrade(id string) (string, error) {
+	objectID, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		err := status.Errorf(codes.InvalidArgument, "Invalid ID format")
+		return "Invalid ID format", err
+	}
+
+	// da li postoji ocena
+	_, err = service.GradeRepository.GetGradeById(objectID)
+	if err != nil {
+		err := status.Errorf(codes.NotFound, "There is no grade with that ID")
+		return "There is no grade with that ID", err
+	}
+
+	// brisanje ocene
+	_, err = service.GradeRepository.DeleteUserGrade(objectID)
+	if err != nil {
+		err := status.Errorf(codes.Internal, "Failed to delete grade")
+		return "Failed to delete grade", err
+	}
+
+	return "Successfully deleted grade", nil
 }
